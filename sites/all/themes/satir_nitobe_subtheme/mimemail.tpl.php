02fea55bdab8675e43e4912affd756db2d4fa0a3
rfay <randy@randyfay.com>
2010-06-18 12:10:44 -0600

    mimemail themeing

---
+++ sites/all/themes/satir_nitobe_subtheme/mimemail.tpl.php
@@ -0,0 +1,26 @@
+<?php // $Id: mimemail.tpl.php,v 1.2 2009/08/10 17:53:39 vauxia Exp $
+
+/**
+ * @file mimemail.tpl.php
+ */
+?>
+<html>
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <?php if ($css): ?>
+    <style type="text/css">
+      <!--
+      <?php print $css ?>
+      -->
+    </style>
+    <?php endif; ?>
+  </head>
+  <body id="mimemail-body">
+    <div id="center">
+      <div id="main">
+        <h1>THIS IS THE MIMEMAIL ALETERE</h1>
+        <?php print $body ?>
+      </div>
+    </div>
+  </body>
+</html>
