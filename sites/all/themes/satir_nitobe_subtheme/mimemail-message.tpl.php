88c8e01108dcd2963d6bfe62205f22d317fb72fa
Randy Fay <rfay@hardy1.thefays.us>
2010-06-22 18:16:27 -0600

    simplenews theming

--- sites/all/themes/satir_nitobe_subtheme/mimemail-message.tpl.php
+++ sites/all/themes/satir_nitobe_subtheme/mimemail-message.tpl.php
@@ -8,18 +8,43 @@
 <html>
   <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <?php if ($css): ?>
     <style type="text/css">
-      <!--
-      <?php print $css ?>
-      -->
+    .left-gutter, .right-gutter {
+       width: 10%;
+       background-image:url('<?php print url("sites/all/themes/satir_nitobe_subtheme/images/background.gif", array('absolute' => TRUE)) ?>');
+       background-color: #CCCCCC;
+    }
+    .simplenews-header {
+       height: 118px;
+       /* margin-top: 4px; */
+       /* margin-bottom: 4px; */
+       background-color: #CCCCCC;
+       background-image:url('<?php print url("sites/all/themes/satir_nitobe_subtheme/images/background.gif", array('absolute' => TRUE)) ?>');
+    }
+    .simplenews-header div, .simplenews-body div.center-image {
+       text-align: center;
+    }
+    .center-text {
+       padding: 10px;
+    }
+      
     </style>
-    <?php endif; ?>
   </head>
   <body id="mimemail-body">
     <div id="center">
       <div id="main">
-        <?php print $body ?>
+  <table width="100%">
+  <tr class="simplenews-header"><td colspan="3"> 
+       <div>
+          <img src="<?php print url("sites/all/themes/satir_nitobe_subtheme/images/satir-header-people.jpg", array('absolute' => TRUE)) ?>">
+       </div>
+	</td></tr>
+  <tr class="simplenews-body">
+    <td class="left-gutter" ></td>
+    <td class="center-text"><?php print $body ?></td>
+    <td class="right-gutter"> </td>
+  </tr>
+ </table>
       </div>
     </div>
   </body>
